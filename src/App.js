dd4e94b8b0c5e536fa4f44b967b7932016e90c30
Vecarope <vecarope@gmail.com>
2022-04-14 23:35:28 -0400

    version1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
+import './components/Navbar'
+import Navbar from './components/Navbar';
 
-function App() {}
+function App() {
+    return ( 
+        <div className = "app" >
+        <Navbar/>
+        <div className="contenedor"><h1>Bienvenido a mi portafolio</h1>
+        </div>
+        </div>
+    )
+
+}
 
 export default App;
