89e703bbc077e0450e506dc9278b8470cbb3add4
Vecarope <vecarope@gmail.com>
2022-04-20 17:13:15 -0400

    version1.4

---
+++ src/components/Carrusel.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import '../styleSheet/Carrusel.css';
+
+const Carrusel = (props) => {
+    const {children} = props
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [length, setLength] = useState(children.length)
+
+    const [touchPosition, setTouchPosition] = useState(null)
+
+    useEffect(() => {
+        setLength(children.length)
+    }, [children])
+
+    const next = () => {
+        if (currentIndex < (length - 1)) {
+            setCurrentIndex(prevState => prevState + 1)
+        }
+    }
+
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+        }
+    }
+
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+    }
+
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition
+
+        if(touchDown === null) {
+            return
+        }
+
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+
+        if (diff > 5) {
+            next()
+        }
+
+        if (diff < -5) {
+            prev()
+        }
+
+        setTouchPosition(null)
+    }
+
+    return (
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+                {
+                    currentIndex > 0 &&
+                    <button onClick={prev} className="left-arrow">
+                        &lt;
+                    </button>
+                }
+                <div
+                    className="carousel-content-wrapper"
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                >
+                    <div
+                        className="carousel-content"
+                        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                    >
+                        {children}
+                    </div>
+                </div>
+                {
+                    currentIndex < (length - 1) &&
+                    <button onClick={next} className="right-arrow">
+                        &gt;
+                    </button>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Carrusel;
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../App.css'; 
 import '../styleSheet/Skill.css'; 
+import Carrusel from "./Carrusel"
 
 
 
@@ -8,9 +9,19 @@ function Skill() {
     return(
         <div className='skill-container'>
         <h1>Mis Skills</h1>
+        
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+            <Carrusel>
+                <img src="public/logos/58" alt="placeholder" />
+                <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+                <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+                <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+                <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+                <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+            </Carrusel>
+        </div>
         </div>
 );
 }
-
 
 export default Skill;
